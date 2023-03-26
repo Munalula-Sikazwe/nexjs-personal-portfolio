19fde0ae241ec836baaa2e7c608205a2f9004fd4
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-03-26 20:10:07 +0200

    changed title name.

--- my-personal-portfolio/src/pages/index.js
+++ my-personal-portfolio/src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home(props) {
   return (
     <>
       <Head>
-        <title>Rene Bitter &lt;Web Developer /&gt;</title>
+        <title>Munalula Sikazwe &lt; Computer Scientist /&gt;</title>
         <meta
           name='description'
           content='My personal web development portfolio including various frontend and fullstack projects as well as web development blog articles. Tech-Stack: React, Next.js, Redux, Typescript, Node.js, Express, MongoDB, Bootstrap, Shopware.'
